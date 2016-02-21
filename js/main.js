27ab1da6f59292a728f9af1c25ca82c8dbf2f153
caitread <creardon3@gmail.com>
2016-02-21 17:40:29 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+for(i=1; i<=100; i++){
+if (i != 3 & i !=5){
+	console.log(i);
+	document.write(i + ",");
+	}
+document.write()
+if (i % 3 === 0){
+    console.log("fizz");
+    document.write("fizz,");
+	}
+if (i % 5 === 0){
+	console.log("buzz");
+	document.write("buzz,");
+	}
+}
